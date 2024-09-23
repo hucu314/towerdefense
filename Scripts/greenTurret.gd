e3c538d2f4de62e16793e56a67be4bc2ea875675
hucu314 <hcumpean@ftstudent.org>
2024-09-23 13:52:23 -0400

    At least it runs

--- Scripts/greenTurret.gd
+++ Scripts/greenTurret.gd
@@ -1,11 +1,8 @@
 extends Sprite2D
-<<<<<<< Updated upstream
 
 var mousePos
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-=======
+
 @onready var timer = $Timer
 
 var mouseAng
@@ -13,17 +10,11 @@ var mouseAng
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.start(5)
->>>>>>> Stashed changes
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-<<<<<<< Updated upstream
-	mousePos = rad_to_deg(get_angle_to(get_global_mouse_position()))
-	self.rotation(mousePos)
-	pass
-=======
 	mouseAng = rad_to_deg(get_angle_to(get_global_mouse_position()))
 	print(mouseAng)
 	pass
@@ -32,4 +23,3 @@ func _process(delta):
 func _on_timer_timeout():
 	self.global_rotation_degrees = mouseAng
 	pass # Replace with function body.
->>>>>>> Stashed changes
