4d3f085a2741d8fba4be18a0264fe97f95b5845f
hucu314 <hcumpean@ftstudent.org>
2024-09-30 06:06:43 -0400

    starting closest enemy tracking
    Started working on it tracking the closest enemy.

--- Scripts/green_turret.gd
+++ Scripts/green_turret.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-var target =  self
-
+var targets = []
+var targetDist = 1000
+var numTargets = 0
+var directions = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,11 +12,43 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var angle = rad_to_deg(self.global_position.angle_to_point(target.global_position))
-	print(angle)
-	rotation_degrees = angle+90
+	var target = closeEnemy()
+	targetDist = target
+	if directions.is_empty()==false:
+		var direction = directions[0]
+		print('Name: '+str(direction[0]))
+		print('Angle: '+str(direction[1]))
+		print('Distance: '+str(direction[2]))
+	directions.clear()
+	if target != null:
+		var angle = rad_to_deg(self.global_position.angle_to_point(target.global_position))
+		rotation_degrees = angle+90
+	pass
+
+func closeEnemy():
+	if targets.is_empty() == false:
+		for x in targets:
+			var angle = rad_to_deg(self.global_position.angle_to_point(x.global_position))
+			var distance = global_position.distance_to(x.global_position)
+			var direction = []
+			direction.append(x.name)
+			direction.append(angle)
+			direction.append(distance)
+			directions.append(direction)
+			#print(directions)
+			#print(global_position.distance_to(x.global_position))
+			#if targetDist >= global_position.distance_to(x.global_position) or numTargets == 1:
+				#targetDist = global_position.distance_to(x.global_position)
+				#return x
 	pass
 
 func _on_area_2d_body_entered(body):
-	target = body
+	targets.append(body)
+	numTargets+=1
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	targets.erase(body)
+	numTargets-=1
 	pass # Replace with function body.
